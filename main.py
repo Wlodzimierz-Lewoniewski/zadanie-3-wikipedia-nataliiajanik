ceb2d9cf30ac5a50d058e6aa215c7f624be02902
Natalia Janik <nataliia.janik@gmail.com>
2024-11-06 20:41:08 +0100

    funciton

--- main.py
+++ main.py
@@ -1,9 +1,77 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+import requests
+import re
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
+def ekstrakcja_danych_z_artykulu(url_suffix):
+    url = f"https://pl.wikipedia.org/wiki/Kategoria:{url_suffix}"
+    response = requests.get(url)
+    
+    html_content = response.text
+    
+    #with open("zawartosc.html", "w", encoding="utf-8") as file:
+    #    file.write(html_content)
+    
+    pattern = r'mw-category-generated".*?<li><a href="([^"]+)" title="[^"]+">.*?</a></li>\s*<li><a href="([^"]+)" title="[^"]+">.*?</a></li>'
+    matches = re.search(pattern, html_content, re.DOTALL)
+    links = []
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+    if matches:
+        links.append(matches.group(1))
+        links.append(matches.group(2))
+        
+        
+    for link in links:
+        article_url = f"https://pl.wikipedia.org/{link}"
+        article_response = requests.get(article_url)
+        article_content = article_response.text
+        
+        #internal links
+        positionInternal = article_content.find('id="mw-content-text"')
+        html_internal_links = article_content[positionInternal:]
+        
+        pattern_internal_links = r'href="[^:]+?".+?title="(.*?)"'
+        internal_links_names = re.findall(pattern_internal_links, html_internal_links)[:5]
+        
+        if internal_links_names:
+            print(" | ".join(internal_links_names))
+        else:
+            print("\n")
+        
+        #imgs
+        img_position = article_content.find('mw-content-ltr mw-parser-output')
+        html_imgs = article_content[img_position:]
+    
+        pattern_imgs = r'<img.+?src="(.+?)"'
+        imgs_links = re.findall(pattern_imgs, html_imgs)[:3]
+        
+        if imgs_links:
+            print(" | ".join(imgs_links))
+        else:
+            print("\n")       
+            
+        #external links
+        external_links_position = article_content.find('id="Przypisy"')
+        if external_links_position is not None:
+            external_links_position = article_content.find(r'mw-references-wrap')
+            html_external_links = article_content[external_links_position:]
+            pattern_external_links = r'"(http.+?)"'
+            external_links = re.findall(pattern_external_links, html_external_links)[:3]
+            if external_links:
+                print(" | ".join(external_links))
+        else:
+            print("\n")
+        
+        #categories
+        categories_position = article_content.find('mw-normal-catlinks')
+        html_categories = article_content[categories_position:]
+    
+        pattern_categories = r'<li.+?>(.+?)</a></li>'
+        categories_links = re.findall(pattern_categories, html_categories)[:3]
+        
+        if categories_links:
+            print(" | ".join(categories_links))
+        else:
+            print("\n")   
+            
+article_name = input()
+article_name = article_name.replace(" ", "_")
+ekstrakcja_danych_z_artykulu(article_name)
